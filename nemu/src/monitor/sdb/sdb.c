25995f9bab4e922e9e4160c98a43fff4101c48ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-12 20:59:11 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:11 up  2:55,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,7 +70,7 @@ static int cmd_info(char *args) {
 static int cmd_x(char *args){
   int N;
   vaddr_t addr;
-  sscanf(args, "%d %lx", &N,&addr);
+  sscanf(args, "%d 0x%lx", &N,&addr);
   for(int i=0; i < N; i++) {
     printf("0x%lx = %ld\n",addr,vaddr_read(addr,4));
     addr += 4;
