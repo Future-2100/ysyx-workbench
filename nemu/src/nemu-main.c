6c05d39236e8a062e81b89f2a7bd9c31804e6434
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 17:52:47 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:52:47 up  3:26,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -8,7 +8,7 @@ int is_exit_status_bad();
 word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
-  /*
+  
   // Initialize the monitor.
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -17,34 +17,32 @@ int main(int argc, char *argv[]) {
 #endif
 
   // Start engine. 
-  engine_start();
+ // engine_start();
 
-  return is_exit_status_bad();
-  */
+//  return is_exit_status_bad();
+ 
 
 
   FILE *fp=NULL;
   fp = fopen("./input","r");
   char buff[1024];
-  //char str[1024] = "1*2+3*5-(4+5)";
   int a=0,b=0;
   word_t result=0,answer=0;
     
   bool success = true;
 
-  //for(int i=0; i<TEST_NUM; i++) {
+  for(int i=0; i<TEST_NUM; i++) {
        a =  fscanf(fp, "%ld", &result);
        b =  fscanf(fp, "%s" , buff);
-    //if(a!=EOF && b!=EOF) {
-    printf("a=%d, b=%d\n",a,b);
-      //answer = expr(str,&success);
-      answer = 20;
+    if(a!=EOF && b!=EOF) {
+      printf("a=%d, b=%d\n",a,b);
+      answer = expr(buff,&success);
       if(success == true && answer==result)
         printf("success\n");
       else
         printf("error\n");
-   //}
-  //}
+    }
+  }
 
   fclose(fp);
   return 0;
