4fcf4ec184bf5d2844878060e42869013201e4d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-16 02:45:47 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:45:47 up  7:01,  0 users,  load average: 0.21, 0.06, 0.02

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -138,6 +138,7 @@ void info_w() {
   WP *p = head.next;
   while(p->next !=NULL) {
     printf("watchpoint NO.%d : %s = (%ld,%ld)\n",p->NO,p->expr,p->result_pre,p->result_now);
+    p = p->next;
   }
 }
 
