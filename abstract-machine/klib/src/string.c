1bcbfa8d810f136b2670e700ef6b046fb55e7bd7
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-19 03:58:44 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:58:44 up  1:32,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -148,8 +148,8 @@ int memcmp(const void *s1, const void *s2, size_t n) {
   size_t i;
 
   for(i=0; i<n; i++) {
-    if ( *(unsigned char *)(s1+n) < *(unsigned char *)(s2+n) ) return -1;
-    if ( *(unsigned char *)(s1+n) > *(unsigned char *)(s2+n) ) return  1;
+    if ( *(unsigned char *)(s1+i) < *(unsigned char *)(s2+i) ) return -1;
+    if ( *(unsigned char *)(s1+i) > *(unsigned char *)(s2+i) ) return  1;
   }
 
 
