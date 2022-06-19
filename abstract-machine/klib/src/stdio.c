39e5f1cfb12e07c11fcba98163b146f2f09af891
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-19 13:22:36 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:22:36 up  3:45,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,6 +6,62 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+
+  //putch(char ch);
+  va_list valist;
+  va_start(valist, fmt);
+  size_t i = 0;
+  size_t j = 0;
+
+  while( *(fmt+i) != '\0' ) {
+
+    if( *(fmt+i) == '%' && *(fmt+i+1) == 'd' ) {
+      int bits=0;
+      int k;
+      int tens= 1;
+
+      int integer = va_arg(valist, int); //352
+        while( integer / (tens) != 0 ) {
+          bits++;
+          tens = tens * 10 ;
+        }
+
+        tens = tens / 10;
+
+        for( k=0; k<bits; k++ ) {
+          char num = (char)(integer / tens + 48 ); 
+          integer = integer % tens ;
+          putch(num);
+          tens = tens / 10;
+        }
+
+        i++;
+    }
+
+
+    else if( *(fmt+i) == '%' && *(fmt+i+1) == 's' ) {
+      char *str = va_arg(valist, char * ) ;
+        while( *(str+j) != '\0' ) {
+          putch(*(str+j));
+          j++;
+        }
+      i++;
+    }
+
+
+    else {
+      putch(*(fmt+i));
+    }
+    
+    i++;
+  }
+
+
+  
+
+  va_end(valist);
+  
+  
   panic("Not implemented");
 }
 
