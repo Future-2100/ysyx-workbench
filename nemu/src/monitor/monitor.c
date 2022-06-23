f94dbe7ca2a37d01191fa3234f883e3e06678a58
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 00:49:29 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:49:29 up 15:01,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -151,9 +151,15 @@ void read_elf(char *elf_file){
       if (fread(&elf_shd[i].sh_addralign, 8, 1, elf_fp) );
       if (fread(&elf_shd[i].sh_entsize  , 8, 1, elf_fp) );
     }
-    Elf64_Off shstrtab_off = elf_shd[e_shstrndx].sh_offset;
-    Log("e_shstrndx.sh_offset = %lx", shstrtab_off);
-    Log("e_strtab.sh_offset = %lx", elf_shd[e_shstrndx-1].sh_offset);
+    Elf64_Off strtab_off = elf_shd[e_shstrndx-1].sh_offset;
+   // Log("e_shstrndx.sh_offset = %lx", shstrtab_off);
+   // Log("e_strtab.sh_offset = %lx", elf_shd[e_shstrndx-1].sh_offset);
+   fseek(elf_fp, strtab_off, SEEK_SET);
+   char str[30];
+   if (fscanf( elf_fp,"%s", str) ) ;
+   Log("%s",str);
+
+    
 
   }
   Log("Elf is read from %s", elf_file ? elf_file : "none");
