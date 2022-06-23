5f19f270c02340ea6c9e74e33c7003eb2bc4d42a
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 20:02:11 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:11 up 10:13,  0 users,  load average: 0.06, 0.03, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -96,9 +96,8 @@ void read_elf(char *elf_file){
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp = fp;
 
-    elf_fp ++;
     char str[4];
-    if( fread(str, 1, 3, elf_fp) != 0 ) {
+    if( fread(str, 1, 4, elf_fp) != 0 ) {
       Log("Elf : %s", str);
     }
 
