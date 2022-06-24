a8e781b2ed753a78df2ff2e3f841f42d738cc914
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 21:21:06 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:06 up 10:46,  0 users,  load average: 0.08, 0.03, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -91,6 +91,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   
+  //record the information in iringbuf
   strcpy( iring_head->iringbuf, s->logbuf );
   iring_head->used = true;
   iring_end = iring_head;
