530554c9d86db1af8414bacfd8c79846f019f05d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 18:40:25 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:40:25 up  8:06,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -112,7 +112,7 @@ void init_elf(char *elf_file){
         symtab_name[i][j] = buf;
         j++;
       }
-      printf("symtab_name[%d]=%s\n", i, symtab_name[i]);
+      printf("symtab[%d]:name %s,  info %d\n", i, symtab_name[i],symtab[i].st_info);
     }
 
 
