a0da33368585779259bf31a3acbf6dc1a8c88154
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 11:33:03 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:33:03 up  1:33,  0 users,  load average: 0.16, 0.06, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -74,6 +74,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
   //record the infomation of the instruction in s->logbuf
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+  printf("test: inst.val = %08x\n", s->isa.inst.val);
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
@@ -98,6 +99,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   iring_head = iring_head->next;
 
   //record the information of function call and ret
+  
   
   
 #endif
