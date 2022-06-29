b73739b607b3963c68c63497b4a3975a7948c221
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 23:41:12 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:41:12 up  1:45,  0 users,  load average: 0.44, 0.11, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -14,7 +14,7 @@
 #define NR_IRING 20
 
 extern FUNCT function[0];
-extern int func_num;
+int func_num;
 
 typedef struct iring {
   char iringbuf[128] ;
@@ -117,7 +117,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 }
 
 static void execute(uint64_t n) {
-  printf("func_num = %d\n", func_num);
+//  printf("func_num = %d\n", func_num);
   Decode s;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
