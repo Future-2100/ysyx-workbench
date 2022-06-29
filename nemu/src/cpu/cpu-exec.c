24203abf2b18782c8bb1b8ae7e0c69ad110c979d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 00:42:52 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:42:52 up  2:47,  0 users,  load average: 0.02, 0.05, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -105,11 +105,13 @@ static void exec_once(Decode *s, vaddr_t pc) {
   if( s->isa.inst.val==0x8067 ) {
     word_t ret_addr = cpu.gpr[1];
     for(i = 0; i < func_num; i++) {
-      if( (ret_addr >= function[i].addr_sta) && (ret_addr < function[i].addr_end) )
+      if( (ret_addr >= function[i].addr_sta) && (ret_addr < function[i].addr_end) ) {
         printf("ret [%s]\n", function[i].name);
-      else
-        printf("ret [???]\n");
+        break;
+      }
     }
+    printf("ret [???]\n");
+
   }
 
   /*
