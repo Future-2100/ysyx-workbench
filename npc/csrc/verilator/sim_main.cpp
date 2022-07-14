ddd9bb1cc786141fca79a9f24239b62ad87ca124
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 20:06:49 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:49 up 11:51,  0 users,  load average: 0.00, 0.01, 0.00

--- npc/csrc/verilator/sim_main.cpp
+++ npc/csrc/verilator/sim_main.cpp
@@ -31,20 +31,23 @@ int main(int argc, char** argv, char** env) {
   // Simulated until $finish
   while( !Verilated::gotFinish() ) {
 
-    // Evaluate model
     if(  top->clk ) {
       if(top->ebreak)  end_sim(); 
-      //contextp->timeInc(1); // 10 timeprecision period passes...
+      contextp->timeInc(1); // 10 timeprecision period passes...
       top->inst = pmem_read(top->pc);
+      top->eval();
+      contextp->timeInc(9);
     }
     
     else {
       printf("pc = %lx, inst = %x \n", top->pc, top->inst);
+      contextp->timeInc(10);
     }
 
-    top->eval();
-    contextp->timeInc(10);
     top->clk = !top->clk ;
+
+    // Evaluate model
+    top->eval();
 
   }
 
