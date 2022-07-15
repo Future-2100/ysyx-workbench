b8d8a341d8bad5706303200f39624eeff927beb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 22:10:22 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:22 up  4:20,  0 users,  load average: 0.06, 0.04, 0.00

--- npc/csrc/verilator/operate.cpp
+++ npc/csrc/verilator/operate.cpp
@@ -38,7 +38,7 @@ void init_module() {
 void run_step(uint64_t n) {
 
   uint64_t m = 2*n;
-  printf("m = %ld\n",m );
+  printf("m = %ld\n", Verilated::gotFinish() );
   while( m-- & ( !Verilated::gotFinish() )  ) {
 
     if(  top->clk ) {
