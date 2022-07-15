e53d609bddba2fb887fc261e3abd98b08d4cc574
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 21:56:10 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:10 up  4:06,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/verilator/monitor.cpp
+++ npc/csrc/verilator/monitor.cpp
@@ -69,7 +69,7 @@ static long load_img() {
 }
 
 static void welcome() {
-  printf("Welcome to" FONT_YELLOW ANSI_BG_RED "riscv64" FONT_NONE "-NPC!\n");
+  printf(FONT_BLUE "Welcome to " FONT_YELLOW ANSI_BG_RED "riscv64" FONT_BLUE "-NPC!" FONT_NONE "\n");
 }
 
 void init_monitor(int argc, char *argv[]) {
