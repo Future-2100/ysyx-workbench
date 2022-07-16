b4da14599c79c4aea446088e2c91c5356fe2ce52
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 23:42:24 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:42:24 up  7:35,  0 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/verilator/disasm.cc
+++ npc/csrc/verilator/disasm.cc
@@ -85,9 +85,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
   
-  printf("1\n");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
-  printf("2\n");
 
   std::string s;
   raw_string_ostream os(s);
