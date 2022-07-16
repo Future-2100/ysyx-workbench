3e10a9d82d2e8516d1ff3fea1eb3305372b8f0b6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 15:38:15 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:38:15 up  4:56,  0 users,  load average: 0.01, 0.03, 0.03

--- npc/csrc/verilator/monitor.cpp
+++ npc/csrc/verilator/monitor.cpp
@@ -18,7 +18,6 @@ uint64_t mem_read(uint64_t addr) {
 }
 
 void mem_write(uint64_t addr, int len, word_t data) {
-      printf("addr = %lx, len = %d, data = %lx\n", addr, len, data);
       if( addr < 0x80000000 || addr >= 0x88000000 ) {
         printf(FONT_RED "address is out of the boundary!\n" FONT_NONE);
         return ;
