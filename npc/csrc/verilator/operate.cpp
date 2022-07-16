c28e45651a6dcd44b1429c7db1595822b079f0a7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 17:06:46 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:46 up 59 min,  0 users,  load average: 0.08, 0.04, 0.03

--- npc/csrc/verilator/operate.cpp
+++ npc/csrc/verilator/operate.cpp
@@ -89,7 +89,7 @@ void run_step(uint64_t n) {
       }
       //top->eval();
       //contextp->timeInc(1); // 10 timeprecision period passes...
-      top->inst = inst_read(top->pc);
+      top->inst = inst_read(top->dnxt_pc);
       top->eval();
       contextp->timeInc(10);
 
