77dceb048ced71d09be0440fc421fdf4eedb662c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 23:53:00 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:00 up  4:30,  0 users,  load average: 0.14, 0.09, 0.03

--- npc/csrc/verilator/difftest.cpp
+++ npc/csrc/verilator/difftest.cpp
@@ -52,11 +52,11 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 */
   //ref_difftest_init(port);
 
+  printf("ref_so_file = %s\n",ref_so_file);
   ref_difftest_memcpy(0x80000000, pmem, img_size, DIFFTEST_TO_REF);
 
   extern uint64_t *cpu_gpr;
   ref_difftest_regcpy(cpu_gpr, DIFFTEST_TO_REF);
-  printf("ref_so_file = %s\n",ref_so_file);
 }
 
 
