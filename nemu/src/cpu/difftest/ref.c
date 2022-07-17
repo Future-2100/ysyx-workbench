7a06608ea977cb640d97a9e803c94c8afeed3eb8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 21:46:50 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:50 up  2:24,  0 users,  load average: 0.01, 0.05, 0.02

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -4,19 +4,19 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+//  assert(0);
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+ // assert(0);
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  //assert(0);
 }
 
 void difftest_raise_intr(word_t NO) {
-  assert(0);
+ // assert(0);
 }
 
 void difftest_init(int port) {
