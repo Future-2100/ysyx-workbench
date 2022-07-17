076c06bd64611b697cf380b96a50585b4d8ecb90
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 23:55:12 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:55:12 up  4:33,  0 users,  load average: 0.04, 0.07, 0.02

--- npc/csrc/verilator/difftest.cpp
+++ npc/csrc/verilator/difftest.cpp
@@ -3,7 +3,7 @@
 
 CPU_state cpu = {};
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = {};
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
