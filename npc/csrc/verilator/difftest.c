1c3b721d54f86bef5ec5694080531657de1dab51
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 22:40:34 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:40:34 up 49 min,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/verilator/difftest.c
+++ npc/csrc/verilator/difftest.c
@@ -23,6 +23,8 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
   }
 }
 
+  extern uint8_t *pmem;
+
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
 
@@ -49,9 +51,8 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   Log("The result of every instruction will be compared with %s. ", ref_so_file);
   ref_difftest_init(port);
 
-  extern uint8_t *pmem;
   printf(FONT_RED "2ref_so_file = %s" FONT_NONE "\n" ,ref_so_file );
-  ref_difftest_memcpy(0x80000000, pmem, img_size, DIFFTEST_TO_REF);
+  ref_difftest_memcpy(0x80000000, (uint8_t *)pmem, img_size, DIFFTEST_TO_REF);
 
   //printf(FONT_RED "2ref_so_file = %s" FONT_NONE "\n" ,ref_so_file );
   extern uint64_t *cpu_gpr;
