785009cb876c7fd2e2430217f6cc54ea1598f872
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 23:12:03 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:03 up  1:20,  0 users,  load average: 0.01, 0.04, 0.03

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -31,6 +31,7 @@ void difftest_regcpy(void *dut, bool direction) {
 }
 
 void difftest_exec(uint64_t n) {
+  printf("!!!!!!!!!!!!!!!!!!!!!!!" );
   cpu_exec(n);
   //assert(0);
 }
--- npc/csrc/verilator/difftest.c
+++ npc/csrc/verilator/difftest.c
@@ -132,6 +132,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     is_skip_ref = false;
     return;
   }
+  printf("~~~~~~~~~~\n");
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
   checkregs(&ref_r, pc);
