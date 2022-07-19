9350d87fe6033cf2c521a11a66ba9f9d9e21fef9
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 19:08:54 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:08:54 up  2:39,  0 users,  load average: 0.32, 0.07, 0.02

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -7,6 +7,8 @@
   enum { AM_##reg = (id) }; \
   typedef struct { __VA_ARGS__; } AM_##reg##_T;
 
+//AM_INPUT_KEYBRD = (8)
+
 AM_DEVREG( 1, UART_CONFIG,  RD, bool present);
 AM_DEVREG( 2, UART_TX,      WR, char data);
 AM_DEVREG( 3, UART_RX,      RD, char data);
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t code = inl( 0xa0000060 );
+  uint32_t code = inl( RTC_ADDR );
   if(code == AM_KEY_NONE) {
     kbd->keydown = false;
     kbd->keycode = 0;
