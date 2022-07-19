d5fe658b54b95962012d5e522cf83470819b4938
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 18:54:36 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:36 up  2:24,  0 users,  load average: 0.44, 0.12, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t code = inl( 0xa0000060 );
+  uint32_t code = inl( 0x80000060 );
   if(code == AM_KEY_NONE) {
     kbd->keydown = false;
     kbd->keycode = 0;
