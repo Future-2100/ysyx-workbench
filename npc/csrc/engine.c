32ed2deaf09b548261be8e93af04b01c68fe4411
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 15:51:30 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:51:30 up  4:55,  0 users,  load average: 0.00, 0.03, 0.00

--- npc/csrc/engine.c
+++ npc/csrc/engine.c
@@ -15,6 +15,7 @@ void sdb_set_batch_mode() {
 }
 
 static int cmd_c(char *args) {
+  printf(ANSI_FMT_RED "---------------------" ANSI_FMT_NONE);
   cpu_exec(-1);
   return 0;
 }
@@ -33,6 +34,7 @@ static int cmd_si(char *args) {
   else
     sscanf(arg, "%d", &n);
 
+  printf(ANSI_FMT_RED "---------------------" ANSI_FMT_NONE);
   cpu_exec(n);
 
   return 0;
