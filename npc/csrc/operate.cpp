41919b656c69a678f7f09362719c971aa1552af3
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 20:23:07 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:07 up  9:27,  0 users,  load average: 0.03, 0.08, 0.22

--- npc/csrc/operate.cpp
+++ npc/csrc/operate.cpp
@@ -90,10 +90,10 @@ void run_step(Decode *s, CPU_state *cpu) {
       }
 
       top->clk = !top->clk;
-      top->eval();
-      contextp->timeInc(1); // 10 timeprecision period passes...
+      //top->eval();
+      //contextp->timeInc(1); // 10 timeprecision period passes...
       top->inst = inst_fetch(&top->pc, 4);
-      contextp->timeInc(9);
+      contextp->timeInc(10);
       top->eval();
 
       if( top->ren ) {
