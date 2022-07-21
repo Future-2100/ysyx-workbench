ea0299794c87a224fb6ad05d97d6abcef4c15d63
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 23:52:30 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:52:30 up  4:02,  0 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -15,7 +15,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 32*32, .height = 32*32,
+    .width = 0, .height = 0,
     .vmemsz = 0
   };
   outb( VGACTL_ADDR , cfg->width << 16 | cfg->height ) ;
