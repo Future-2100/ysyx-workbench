a7f8f4af72a404cdc07b2899673a91b0a07b6cad
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 11:16:55 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:55 up  2:13,  0 users,  load average: 0.03, 0.05, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,25 +5,41 @@
 
 void __am_gpu_init() {
   int i;
-  int w= 32;
-  int h= 32;
+  uint32_t vga_config = inl(VGACTL_ADDR);
+  int w = (vga_config >>  16 );
+  int h = (vga_config && 0xff);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+
+  uint32_t vga_config = inl(VGACTL_ADDR);
+  uint32_t width  = (vga_config >>  16 );
+  uint32_t height = (vga_config && 0xff);
+
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 32, .height = 32,
-    .vmemsz = 32*32
+    .width = width , .height = height,
+    .vmemsz = width * height
   };
-  outb( VGACTL_ADDR , cfg->width << 16 | cfg->height ) ;
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  uint32_t vga_config = inl(VGACTL_ADDR);
+  int WIDTH = (vga_config >>  16 );
+  uint64_t first_addr = FB_ADDR + (WIDTH * ctl->y) + ctl->x;
+  uint64_t addr = first_addr;
+  for ( int j = 0; j < ctl->h; j++) {
+    for ( int i = 0; i < ctl->w; i++) {
+      outl(  (uintptr_t)addr , *(uint32_t *)(ctl->pixels) );
+      addr ++ ;
+    }
+    addr += (WIDTH - ctl->w);
   }
 }
 
