d0c10f141e42f37f700a2c949e0240c8af3eaac2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 14:38:27 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:38:27 up  5:39,  0 users,  load average: 0.08, 0.05, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,12 +5,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("c->mcause  = %d\n", c->mcause);
-  printf("c->mstatus = %d\n", c->mstatus);
- // printf("c->mepc    = %x\n", c->mepc);
-  for( int i=0; i<32; i++ ) {
-    printf("c->gpr[%d] = %d\n", i,c->gpr[i]);
-  }
+
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
