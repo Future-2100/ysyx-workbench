2756b406e9a3b07099ce340ea068a94d24f3f394
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 17:25:09 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:09 up  8:26,  0 users,  load average: 0.25, 0.13, 0.04

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -24,7 +24,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
 }
 
 void init_ramdisk() {
-  Log("ramdisk info: start = %d, end = %d, size = %d bytes",
+  Log("ramdisk info: start = %p, end = %p, size = %p bytes",
       &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
