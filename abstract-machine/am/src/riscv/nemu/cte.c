d35d409e1505048dff9acd7b531f18c0ecb8ab33
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 22:50:12 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:50:12 up  6:30,  0 users,  load average: 0.00, 0.01, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -2,9 +2,23 @@
 #include <riscv/riscv.h>
 #include <klib.h>
 
+/*
+struct Context {
+  // TODO: fix the order of these members to match trap.S
+
+  uintptr_t gpr[32], mcause, mstatus, mepc;
+
+  void *pdir;
+};
+*/
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  printf(" mcause  = %x\n", c->mcause );
+  printf(" mstatus = %x\n", c->mstatus);
+  printf(" mepc    = %x\n", c->mepc   );
+  for( int i = 0; i < 32; i ++)
+    printf(" gpr[%d]    = %x\n", i, c->gpr[i]   );
 
   if (user_handler) {
     Event ev = {0};
