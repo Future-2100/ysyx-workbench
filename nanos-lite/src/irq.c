56683c3b78d97355bdb1f68a29881a964f6a5ed8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 17:12:20 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:12:20 up 52 min,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -10,11 +10,13 @@ struct Context {
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
       case EVENT_YIELD     :  printf("here is the event_yield !\n") ; break; 
+                              /*
       case EVENT_SYSCALL   : break;
       case EVENT_PAGEFAULT : break;
       case EVENT_ERROR     : break;
       case EVENT_IRQ_TIMER : break;
       case EVENT_IRQ_IODEV : break;
+      */
       default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,6 +25,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
+//  naive_uload(NULL, NULL);
 
 }
 
