a68e955ccba88c68427a1839ede1e483d495f543
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 19:58:53 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:58:53 up  3:38,  0 users,  load average: 0.02, 0.16, 0.09

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -11,7 +11,7 @@ extern uint8_t ramdisk_end;
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
-  printf("RAMDISK_SIZE = %d\n", RAMDISK_SIZE);
+  printf("RAMDISK_SIZE = %x\n", RAMDISK_SIZE);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
