26ceffd6f3f015734be9423ae7dff6c8a8f711c6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 15:25:08 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:08 up  5:52,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,216 +5,134 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+static char number_buf[128];
 
+void change_format_x(uint64_t x_number) {
+  int i;
+  if( x_number == 0 ) {
+    number_buf[0] = '0' ;
+    number_buf[1] = '0' ;
+    number_buf[2] = '\0';
+    return ;
+  }
+  else if ( x_number == 0xffffffffffffffff ) {
+    number_buf[0] = '0' ;
+    for( i=1; i<17; i++ )  number_buf[i] = 'f';
+    number_buf[i] = '\0';
+  
+  }
+  else {
+    uint64_t system = 1ull;
+    uint64_t bits   = 1ull;
+    while( x_number / system != 0ull ) {
+      bits ++ ;
+      system = system * 16ull ;
+      if(bits == 17 )  { system = 0xffffffffffffffff; break; }
+    }
+    bool modefied = false ;
+    for( i=0; i<bits; i++ ) {
+      uint64_t bit_number = x_number / system ;
+      if(bit_number < 10ull )  number_buf[i] = bit_number + '0' ;
+      else                 number_buf[i] = bit_number + 'a' - 10 ;
+      x_number = x_number % system ;
+      if(bits == 17 && modefied == false ) {
+        system = 0x1000000000000000;
+        modefied = true;
+      }
+      else { system = system / 16ull; }
+    }
+    number_buf[i] = '\0';
+  }
+}
+
+void change_format_d(int64_t input_number) {
+  int64_t d_number = input_number;
+  int i;
+  if( input_number == 0 ) {
+    number_buf[0] = ' ' ;
+    number_buf[1] = '0' ;
+    number_buf[2] = '\0';
+    return ;
+  
+  }
+  if( d_number < 0 )  {
+    d_number = - d_number ;
+    number_buf[0] = '-' ;
+  
+  }
+  else {
+    number_buf[0] = ' ';
+  
+  }
+  int64_t system = 1;
+  int64_t bits   = 1;
+  while( d_number / system != 0 ) {
+    bits ++ ;
+    system = system * 10 ;
+  }
+  system = system / 10;
+  for( i=1; i<bits; i++ ) {
+    int64_t bit_number = d_number / system ;
+    number_buf[i] = bit_number + '0' ;
+    d_number = d_number % system ;
+    system = system / 10;
+  }
+  number_buf[i] = '\0';
+}
+
+int printf(const char *fmt, ...) {
   va_list valist;
   va_start(valist, fmt);
 
-  int buf[8];
-  int i;
-  unsigned long long int tens ;
-  int allbits;
-  int bits;
-  int integer;
-  int zerobits;
-  int k;
-  char chnum;
-
+  char *char_buf ;
   while( *fmt ) {
-    if( *fmt == '%') {
-      const char *ret = fmt + 1 ;
-
-      switch ( *ret ) {
-
-        case '0' : 
-        case '1' :
-        case '2' :
-        case '3' :
-        case '4' :
-        case '5' :
-        case '6' :
-        case '7' :
-        case '8' :
-        case '9' :
-                    i = 0 ;
-                    tens = 1;
-                    allbits = 0;
-                    while( *ret>= '0' && *ret <= '9') {
-                      buf[i] = *ret - '0' ;
-                      i++ ;
-                      ret ++ ;
-                    }
-                    while(i--) {
-                      allbits = buf[i] * tens + allbits ;
-                      tens = tens * 10;
-                    }
-                    switch ( *ret ) {
-                      case 'd' :
-                                bits=0;
-                                tens= 1;
-                                integer = va_arg(valist, int);
-                                while( integer / (tens) != 0 ) {
-                                  bits++;
-                                  tens = tens * 10 ;
-                                }
-                                tens = tens / 10;
-                                zerobits = allbits - bits;
-                                for(k=0; k<zerobits; k++) {
-                                  putch('0');
-
-                                }
-                                for( k=0; k<bits; k++ ) {
-                                  char num = (char)(integer / tens + '0' ); 
-                                  integer = integer % tens ;
-                                  putch(num);
-                                  tens = tens / 10;
-                                }
-                                fmt = ret ;
-                                break;
-
-                      case 'x' : break ;
-                      default  : break ;
-
-                    }
-                    break;
-
-        case 'c' : char ch = va_arg(valist, int);
-                   putch(ch);
-                   fmt = ret ;
-                   break;
-        case 's' : char *pc = va_arg(valist, char *);
-                   while(*pc) {
-                     putch(*pc);
-                     pc++;
-                   }  
-                   fmt = ret ;
-                   break;
-
-        case 'd' : bits=0ull;
-                   tens= 1ull;
-                   integer = va_arg(valist, unsigned long long );
-                   if( integer == 0ull ) {
-                     putch('0');
-                   }
-                   else {
-                   while( integer / (tens) != 0ull ) {
-                     bits++;
-                     tens = tens * 10ull ;
-                   }
-                   tens = tens / 10ull;
-                   for( int k=0; k<bits; k++ ) {
-                     char num = (char)(integer / tens + '0' ); 
-                     putch(num);
-                     integer = integer % tens ;
-                     tens = tens / 10ull;
-                   }
-                   }
-                   fmt = ret ;
-                   break;
-
-        //case 'p' :
-        case 'x' : bits=0;
-                   tens= 1ull;
-                   integer = va_arg(valist, unsigned int);
-                   while( (integer / tens) != 0 ) {
-                     bits++;
-                     tens = tens * 16 ;
-                   }
-                   tens = tens / 16;
-                   for( k=0; k<bits; k++ ) {
-                     unsigned int num = (unsigned)integer/(unsigned)tens;
-                     if( num >= 10 ) {
-                       chnum = (char)( num + 'a' -10 ); 
-                     }
-                     else {
-                       chnum = (char)( num + '0' ); 
-                     }
-                     putch(chnum);
-                     integer = integer % tens ;
-                     tens = tens / 16;
-                   }
-                   fmt = ret ;
-                   break;
-
-        case 'u' : bits=0;
-                   unsigned int tens= 1;
-                   unsigned int integer = va_arg(valist, unsigned int);
-                   while( integer / (tens) != 0 ) {
-                     bits++;
-                     tens = tens * 10 ;
-                   }
-                   tens = tens / 10;
-                   for( k=0; k<bits; k++ ) {
-                     char num = (char)(integer / tens + '0' ); 
-                     integer = integer % tens ;
-                     putch(num);
-                     tens = tens / 10;
-                   }
-                   fmt = ret ;
-                   break;
-
-        default : putch(*fmt); fmt++; break;
-
+    if( ( *fmt == '%' && *(fmt+1) == 'x' )  ||
+        ( *fmt == '%' && *(fmt+1) == 'p' ) ) {
+      uint64_t x_number = va_arg(valist, uint64_t);
+      change_format_x(x_number);
+      char_buf = number_buf + 1 ;
+      while( *char_buf ) {
+        putch( *char_buf );
+        char_buf++;
       }
+      fmt++;
     }
+
+    if( (*fmt == '%' && *(fmt+1) == 'd') || 
+        (*fmt == '%' && *(fmt+1) == '0'  && *(fmt+2) == '2' && *(fmt+3) == 'd' ) ) {
+      int64_t d_number = va_arg(valist, int64_t);
+      change_format_d(d_number);
+      char_buf = number_buf ;
+      while( *char_buf ) {
+        putch( *char_buf );
+        char_buf++;
+      }
+      fmt++;
+    }
+
+    if( *fmt == '%' && *(fmt+1) == 's' ) {
+      char* string = va_arg(valist, char*);
+        while( *string ){
+          putch( *string );
+          string++;
+        }
+      fmt++;
+    }
+
+    if( *fmt == '%' && *(fmt+1) == 'c' ) {
+      char character = va_arg(valist, int);
+      putch(character);
+      fmt++;
+    }
+
     else {
       putch(*fmt);
     }
-    fmt ++ ;
 
+    fmt++;
   }
-
-/*
-
-
-  while( *(fmt+i) != '\0' ) {
-
-    if( *(fmt+i) == '%' && *(fmt+i+1) == 'd' ) {
-      int bits=0;
-      int k;
-      int tens= 1;
-
-      int integer = va_arg(valist, int); //352
-        while( integer / (tens) != 0 ) {
-          bits++;
-          tens = tens * 10 ;
-        }
-
-        tens = tens / 10;
-
-        for( k=0; k<bits; k++ ) {
-          char num = (char)(integer / tens + 48 ); 
-          integer = integer % tens ;
-          putch(num);
-          tens = tens / 10;
-        }
-
-        i++;
-    }
-
-
-    else if( *(fmt+i) == '%' && *(fmt+i+1) == 's' ) {
-      char *str = va_arg(valist, char * ) ;
-        while( *(str+j) != '\0' ) {
-          putch(*(str+j));
-          j++;
-        }
-      i++;
-    }
-
-
-    else {
-      putch(*(fmt+i));
-    }
-    
-    i++;
-  }
-*/
-
   va_end(valist);
-  
   return 0;
- // panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
