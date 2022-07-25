c0215c0b85eb401712d840f8d4b9b1fe6ce7d070
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 19:22:10 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:10 up  3:01,  0 users,  load average: 0.05, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,36 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf64_Off phoff;
+  ramdisk_read( &phoff, 32, 8);
+  uint16_t phentsize;
+  ramdisk_read( &phentsize, 54, 2);
+  uint16_t phnum;
+  ramdisk_read( &phnum, 56, 2);
+
+  Elf_Phdr elf_phdr;
+  uintptr_t entry = 0;
+  for( int i = 0; i < phnum; i++ ) {
+    ramdisk_read( &elf_phdr.p_type   , phoff + i*phentsize + 0 , 4 );
+    ramdisk_read( &elf_phdr.p_offset , phoff + i*phentsize + 4 , 4 );
+    ramdisk_read( &elf_phdr.p_vaddr  , phoff + i*phentsize + 4 , 8 );
+    ramdisk_read( &elf_phdr.p_paddr  , phoff + i*phentsize + 8 , 8 );
+    ramdisk_read( &elf_phdr.p_filesz , phoff + i*phentsize + 8 , 8 );
+    ramdisk_read( &elf_phdr.p_memsz  , phoff + i*phentsize + 8 , 8 );
+    ramdisk_read( &elf_phdr.p_flags  , phoff + i*phentsize + 8 , 8 );
+    ramdisk_read( &elf_phdr.p_align  , phoff + i*phentsize + 8 , 8 );
+    if( elf_phdr.p_type == PT_LOAD ) {
+      entry = elf_phdr.p_vaddr;
+      ramdisk_read( (char *)elf_phdr.p_vaddr, elf_phdr.p_offset, elf_phdr.p_memsz );
+      memset( (char *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
+    }
+  }
+
+//  TODO();
+  return entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
