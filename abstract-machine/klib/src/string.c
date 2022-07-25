898bc0e74b072d6c6d511b269256db2b79cd61f1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 19:40:52 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:40:52 up  3:20,  0 users,  load average: 0.02, 0.05, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -127,6 +127,8 @@ void *memmove(void *dst, const void *src, size_t n) {
 
 void *memcpy(void *out, const void *in, size_t n) {
   if( out == NULL || in == NULL || out <= in+n ) {
+    printf(" memcpy error ");
+    assert(0);
     return NULL;
   }
   char *tmp_dest = out;
