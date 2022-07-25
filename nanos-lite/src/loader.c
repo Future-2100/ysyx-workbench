af9c48663c1cfbdebf1c8ab487d4df61bba6178f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 20:47:09 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:09 up  4:26,  0 users,  load average: 0.02, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -41,7 +41,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if( elf_phdr.p_type == PT_LOAD ) {
       entry = elf_phdr.p_vaddr;
       ramdisk_read( (char *)(elf_phdr.p_vaddr), elf_phdr.p_offset, elf_phdr.p_memsz );
-      //memset( (char *)(elf_phdr.p_vaddr + elf_phdr.p_filesz), 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
+      memset( (char *)(elf_phdr.p_vaddr + elf_phdr.p_filesz), 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
      }
   }
 
