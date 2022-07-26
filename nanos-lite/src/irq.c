2ee4a058f5ee03471acac758afbae3a199853cb7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 12:44:13 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:44:13 up  3:25,  0 users,  load average: 0.00, 0.01, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,11 +7,12 @@ struct Context {
 };
 */
 
+void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
 
   switch (e.event) {
       case EVENT_YIELD     :  printf("\n\nhere is the event_yield !\n\n") ; break; 
-      case EVENT_SYSCALL   :  printf("\n\nhere is the event_syscall!\n\n"); break;
+      case EVENT_SYSCALL   :  do_syscall(c); break;
                               /*
       case EVENT_PAGEFAULT : break;
       case EVENT_ERROR     : break;
