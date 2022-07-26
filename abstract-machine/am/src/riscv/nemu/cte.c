49de0ac238aaffd79221731d65e2f86ea9edb75f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 20:30:13 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:30:13 up  2:11,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -48,7 +48,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, 1; ecall");
 }
 
 bool ienabled() {
