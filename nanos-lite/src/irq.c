3f2d72102d4a9a53a9f54777ebcc4afbacafbaac
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 21:33:00 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:00 up  3:13,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -12,7 +12,6 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
 
   switch (e.event) {
-      case EVENT_NULL      :  do_syscall(c); break;
       case EVENT_YIELD     :  printf("\n\nhere is the event_yield !\n\n") ; break; 
       case EVENT_SYSCALL   :  do_syscall(c); break;
                               /*
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,10 +9,6 @@ int sys_exit(){
 }
 */
 
-void sys_yield() {
-  yield();
-}
-
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -20,7 +16,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     //case SYS_exit  : printf("is in sys_exti \n"); halt()              break;
-    case SYS_yield : printf("is in sys_yield\n"); sys_yield();  break;
+    case SYS_yield : printf("is in sys_yield\n"); yield();  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx = 0 ;
