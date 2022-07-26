e494a9b7a00fa67f66b676ca0eaea5b3f8df9a9f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 21:38:00 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:38:00 up  3:18,  0 users,  load average: 0.01, 0.01, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,7 +19,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case -1  : ev.event = EVENT_YIELD; break;
-      case EVENT_SYSCALL : ev.event = EVENT_SYSCALL; break;
+      case  2  : ev.event = EVENT_SYSCALL; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
