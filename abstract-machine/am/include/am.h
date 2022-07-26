e4941ce8bc6f1a0002e31f170d642f4b17ae2e8a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 12:35:41 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:35:41 up  3:17,  0 users,  load average: 0.01, 0.03, 0.00

--- abstract-machine/am/include/am.h
+++ abstract-machine/am/include/am.h
@@ -24,7 +24,7 @@ typedef struct Context Context;
 typedef struct {
   enum {
     EVENT_NULL = 0,
-    EVENT_YIELD , EVENT_PAGEFAULT, EVENT_ERROR,EVENT_SYSCALL ,
+    EVENT_YIELD , EVENT_PAGEFAULT, EVENT_ERROR,EVENT_SYSCALL = 4 ,
     EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,
   } event;
   uintptr_t cause, ref;
