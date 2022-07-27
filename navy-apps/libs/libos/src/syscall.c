3e5f395323c05d24aa1db68789dd6a69d7487ca5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 13:08:55 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:08:55 up  3:58,  0 users,  load average: 0.10, 0.11, 0.05

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -48,6 +48,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t ret asm (GPRx);
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
   return ret;
+  putch(ret + '0');
 }
 
 void _exit(int status) {
