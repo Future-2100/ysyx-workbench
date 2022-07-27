f149386815df58bb57cf3e6628da33580067b6c2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 10:52:44 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:44 up  1:42,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,20 +1,30 @@
 #include <common.h>
 #include "syscall.h"
 
-void write( uintptr_t *ret , void *pdir){
-
-  *ret = 0;
+uintptr_t write( int fd, uintptr_t buf, size_t count ) {
+  char *ch = (char *)buf;
+  if( fd==1 || fd==2 ) {
+    for(int i = 0; i < count; i++) {
+      putch(*ch);
+      ch++;
+    }
+  }
+  return count; 
 }
 
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   //printf(" syscall number = %d\n", a[0]);
 
   switch (a[0]) {
     case SYS_exit  : printf("is in sys_exit \n"); halt(c->GPRx) ;  break;
     case SYS_yield : printf("is in sys_yield\n"); yield()       ;  break;
+    case SYS_write : printf("is in sys_write\n"); write((int)a[1],a[2],a[3]); break;
     case  -1       : printf(" here is_yield \n");                  break;
 
     //case SYS_write : printf("is in sys_write\n"); write(&c->GPRx,pdir);  break;
