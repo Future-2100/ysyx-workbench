1d98215c0ad8122ffb78a580cd820e27720ae836
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 10:12:02 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:12:02 up  1:01,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,12 +13,12 @@ void do_syscall(Context *c) {
   //printf(" syscall number = %d\n", a[0]);
 
   switch (a[0]) {
-    case   0 : printf("is in sys_exit \n"); halt(c->GPRx) ;  break;
-    case   2 : printf("is in sys_yield\n"); yield();  break;
-    case  -1 : printf(" here is_yield \n");   break;
+    case SYS_exit  : printf("is in sys_exit \n"); halt(c->GPRx) ;  break;
+    case SYS_yield : printf("is in sys_yield\n"); yield()       ;  break;
+    case  -1       : printf(" here is_yield \n");   break;
 
     //case SYS_write : printf("is in sys_write\n"); write(&c->GPRx,pdir);  break;
-    case SYS_write : printf("is in sys_write\n");  break;
+    //case SYS_write : printf("is in sys_write\n");  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
