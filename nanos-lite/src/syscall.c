1b3fbaa2b49d14931a232d55e01c4b6a6628dd4a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 14:49:37 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:37 up 57 min,  0 users,  load average: 0.02, 0.01, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,7 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 
-uintptr_t write( int fd, uintptr_t buf, size_t count ) {
+uintptr_t sys_write( int fd, uintptr_t buf, size_t count ) {
   char *ch = (char *)buf;
   if( fd==1 || fd==2 ) {
     for(int i = 0; i < count; i++) {
@@ -9,8 +9,13 @@ uintptr_t write( int fd, uintptr_t buf, size_t count ) {
       ch++;
     }
   }
-//  printf("count = %d\n", count);
   return count ; 
+}
+
+
+uintptr_t sys_brk( uintptr_t program_break ) {
+
+  return 0;
 }
 
 void do_syscall(Context *c) {
@@ -21,19 +26,21 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit  : printf("is in sys_exit \n"); 
+    case SYS_exit  : //printf("is in sys_exit \n"); 
                      halt(a[1]) ;  
                      c->GPRx = 0 ;
                      break;
 
-    case SYS_yield : printf("is in sys_yield\n"); 
+    case SYS_yield : //printf("is in sys_yield\n"); 
                      yield() ; 
                      c->GPRx = 0  ;
                      break  ;
 
     case SYS_write : //printf("is in sys_write\n"); 
-                     c->GPRx = ( write((int)a[1],a[2],a[3]) );
+                     c->GPRx = ( sys_write((int)a[1],a[2],a[3]) );
                      break;
+
+    case SYS_brk   : c->GPRx = sys_brk( a[1] );
 
     case  -1       : printf(" here is_yield \n");  
                      c->GPRx = 0 ;
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <time.h>
 #include "syscall.h"
+#include <stdbool.h>
 
 // helper macros
 #define _concat(x, y) x ## y
@@ -65,8 +66,31 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
+extern uintptr_t _end ;
+uintptr_t program_break ;
+static bool break_inited = false ;
+
+/*
+void init_program_break {
+  uintptr_t program_break = _end;
+}
+*/
 void *_sbrk(intptr_t increment) {
-  return (void *)-1;
+  if( break_inited == false ) {
+    program_break = _end;
+    break_inited == true;
+    return (void *)program_break;
+  }
+
+  uintptr_t old_break = program_break ;
+  uintptr_t new_break = program_break + increment ;
+  if ( _syscall_(SYS_brk, program_break, 0, 0) == 0 )  {
+    program_break = new_break ;
+    return (void *)old_break ;
+  }
+  else {
+    return (void *)-1;
+  }
 }
 
 int _read(int fd, void *buf, size_t count) {
