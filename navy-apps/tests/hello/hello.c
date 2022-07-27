2b6189bd2d3d2330c825febd7384c21f762c38bd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 11:42:30 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:30 up  2:31,  0 users,  load average: 0.00, 0.02, 0.00

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,12 +3,12 @@
 
 int main() {
   write(1, "Hello World!\n", 13);
+  write(1, "in printf function!\n", 20);
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 10000) {
-      write(1, "in printf function!\n", 20);
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
