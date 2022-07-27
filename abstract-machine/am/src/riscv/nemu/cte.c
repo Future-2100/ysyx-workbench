69574deb37a0ac8a2b29a6889e113dd4bad17bd7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 09:37:58 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:37:58 up 27 min,  0 users,  load average: 0.58, 0.13, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,8 +19,8 @@ Context* __am_irq_handle(Context *c) {
 #ifdef CONFIG_ETRACE
   printf("---------------- trap triggered -------------------\n");
   printf(" mcause  = %d \n", c->mcause );
-  printf(" mstatus = %d \n", c->mstatus);
-  printf(" mepc    = %d \n", c->mepc   );
+  printf(" mstatus = %x \n", c->mstatus);
+  printf(" mepc    = %x \n", c->mepc   );
   for(int i = 0; i < 32; i++) {
     printf(" gpr[%d]  = 0x%x \n", i, c->gpr[i]);
   }
