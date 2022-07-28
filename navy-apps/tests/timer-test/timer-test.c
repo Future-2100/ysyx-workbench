c9b75a880a3706cf6bc4dbdc44f78ca34f40ecc8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 22:07:49 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:07:49 up  2:15,  0 users,  load average: 0.69, 0.18, 0.06

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -8,7 +8,7 @@ int main() {
   struct timeval tv;
   while(1) {
     gettimeofday(&tv, NULL);
-    if( tv.tv_usec >=500 ) {
+    if( tv.tv_usec ==500 ) {
       printf("output a sentence every 0.5 second . %d\n",times);
       times ++ ;
     }
