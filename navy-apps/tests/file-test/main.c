ae80f0a271fac4a92f07779835a59baa52c54d60
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 16:24:34 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:24:34 up  7:23,  0 users,  load average: 0.06, 0.02, 0.00

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 
-int _open(const char *path, int flags, mode_t mode);
+//int _open(const char *path, int flags, mode_t mode);
 int main() {
-  int i = _open("/share/files/num", 0,0);
+  //int i = _open("/share/files/num", 0,0);
   //printf("begin of the main\n");
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
