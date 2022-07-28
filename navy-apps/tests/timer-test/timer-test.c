0db4151339e9dc3f3a5fa9082be78fb8bdcd8aed
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 22:55:30 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:55:30 up  3:03,  0 users,  load average: 0.14, 0.06, 0.01

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-//uint64_t NDL_GetTicks();
-//int NDL_Init();
+uint64_t NDL_GetTicks();
+int NDL_Init();
 
 int main() {
   NDL_Init();
@@ -14,11 +14,5 @@ int main() {
     seconds = NDL_GetTicks() / 1000000 ;
     printf(" Now time is %d seconds\n", seconds );
 
-    //gettimeofday(&tv, NULL);
-    /*
-    if( tv.tv_usec ==500 || tv.tv_usec == 0) {
-      printf("output a sentence every 0.5 second. %d\n",times);
-      times ++ ;
-      */
   }
 }
