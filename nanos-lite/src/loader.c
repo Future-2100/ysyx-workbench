6c2d43a0d47342dcda8cc3ff92736ceb8b20cdf1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 12:36:09 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:36:09 up  3:35,  0 users,  load average: 0.00, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,28 +25,41 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   fs_read(fd, &phoff, 8);
   //ramdisk_read( &phoff, 32, 8);
   assert( phoff == 0x40 );
-  halt(0);
-  return 0 ;
   
   uint16_t phentsize;
-  ramdisk_read( &phentsize, 54, 2);
+  fs_lseek(fd, 54, 0);
+  fs_read(fd, &phentsize, 2);
+  //ramdisk_read( &phentsize, 54, 2);
 
   uint16_t phnum;
-  ramdisk_read( &phnum, 56, 2);
+  fs_lseek(fd, 56, 0);
+  fs_read(fd, &phnum, 2);
+  //ramdisk_read( &phnum, 56, 2);
 
   Elf64_Addr entry;
-  ramdisk_read( &entry, 24, 8 );
+  fs_lseek(fd, 24, 0);
+  fs_read(fd, &entry, 8);
+  //ramdisk_read( &entry, 24, 8 );
 
   Elf_Phdr elf_phdr;
   for( int i = 0; i < phnum; i++ ) {
-    ramdisk_read( &elf_phdr.p_type   , phoff + phentsize*i +  0 , 4 );
-    ramdisk_read( &elf_phdr.p_flags  , phoff + phentsize*i +  4 , 4 );
-    ramdisk_read( &elf_phdr.p_offset , phoff + phentsize*i +  8 , 8 );
-    ramdisk_read( &elf_phdr.p_vaddr  , phoff + phentsize*i + 16 , 8 );
-    ramdisk_read( &elf_phdr.p_paddr  , phoff + phentsize*i + 24 , 8 );
-    ramdisk_read( &elf_phdr.p_filesz , phoff + phentsize*i + 32 , 8 );
-    ramdisk_read( &elf_phdr.p_memsz  , phoff + phentsize*i + 40 , 8 );
-    ramdisk_read( &elf_phdr.p_align  , phoff + phentsize*i + 48 , 8 );
+    fs_lseek(fd, phoff + phentsize*i, 4);
+    fs_read(fd, &elf_phdr.p_type  , 4);
+    fs_read(fd, &elf_phdr.p_flags , 4);
+    fs_read(fd, &elf_phdr.p_offset, 8);
+    fs_read(fd, &elf_phdr.p_vaddr , 8);
+    fs_read(fd, &elf_phdr.p_paddr , 8);
+    fs_read(fd, &elf_phdr.p_filesz, 8);
+    fs_read(fd, &elf_phdr.p_memsz , 8);
+    fs_read(fd, &elf_phdr.p_align , 8);
+   // ramdisk_read( &elf_phdr.p_type   , phoff + phentsize*i +  0 , 4 );
+   // ramdisk_read( &elf_phdr.p_flags  , phoff + phentsize*i +  4 , 4 );
+   // ramdisk_read( &elf_phdr.p_offset , phoff + phentsize*i +  8 , 8 );
+   // ramdisk_read( &elf_phdr.p_vaddr  , phoff + phentsize*i + 16 , 8 );
+   // ramdisk_read( &elf_phdr.p_paddr  , phoff + phentsize*i + 24 , 8 );
+   // ramdisk_read( &elf_phdr.p_filesz , phoff + phentsize*i + 32 , 8 );
+   // ramdisk_read( &elf_phdr.p_memsz  , phoff + phentsize*i + 40 , 8 );
+   // ramdisk_read( &elf_phdr.p_align  , phoff + phentsize*i + 48 , 8 );
     if( elf_phdr.p_type == PT_LOAD ) {
       ramdisk_read( (char *)(elf_phdr.p_vaddr), elf_phdr.p_offset , elf_phdr.p_memsz );
       memset( (char *)(elf_phdr.p_vaddr + elf_phdr.p_filesz), 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
@@ -54,6 +67,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
 
       Log("program loaded successful" );
+  halt(0);
+  return 0 ;
   return entry ;
 }
 
