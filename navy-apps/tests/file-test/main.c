1b7177be0c559d09f2c864d66cb4a55804c26a7c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 15:35:26 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:35:26 up  6:34,  0 users,  load average: 0.01, 0.06, 0.03

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,20 +2,25 @@
 #include <assert.h>
 
 int main() {
-
+  printf(" >>>>>>>1 \n");
   FILE *fp = fopen("/share/files/num", "r+");
+  printf(" >>>>>>>2 \n");
   assert(fp);
 
+  printf(" >>>>>>>3 \n");
   fseek(fp, 0, SEEK_END);
+  printf(" >>>>>>>4 \n");
   long size = ftell(fp);
   assert(size == 5000);
 
+  printf(" >>>>>>>5 \n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf(" >>>>>>>6 \n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
@@ -35,6 +40,7 @@ int main() {
 
   fclose(fp);
 
+  printf(" >>>>>>>7 \n");
   printf("PASS!!!\n");
 
   return 0;
