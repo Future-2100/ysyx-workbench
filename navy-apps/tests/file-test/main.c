a7e4d224c0212489d6064b40cea88a36efed11d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 15:43:23 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:23 up  6:42,  0 users,  load average: 0.11, 0.08, 0.04

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -3,23 +3,18 @@
 
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
-  printf(" >>>>>>>2 \n");
   assert(fp);
 
-  printf(" >>>>>>>3 \n");
   fseek(fp, 0, SEEK_END);
-  printf(" >>>>>>>4 \n");
   long size = ftell(fp);
   assert(size == 5000);
 
-  printf(" >>>>>>>5 \n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf(" >>>>>>>6 \n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
