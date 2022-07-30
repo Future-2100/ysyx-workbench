2447634a636a36e644f3262abdfb4ab26057a2c7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 10:33:23 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:23 up  1:03,  0 users,  load average: 0.00, 0.01, 0.00

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -24,7 +24,7 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  int fp = open("/dev/events", 0);
+  int fp = _open("/dev/events", 0, 0);
   printf( "fp = %d\n" , fp);
   return read(fp, buf, len);
  // return read( 0 , buf, len);
