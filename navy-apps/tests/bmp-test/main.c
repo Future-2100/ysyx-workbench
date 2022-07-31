24a98cb3f61b6fb6d9974d2dc9577e9aa418ba57
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 11:12:55 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:12:55 up 39 min,  0 users,  load average: 0.09, 0.06, 0.01

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -8,9 +8,9 @@ int main() {
   NDL_Init(0);
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf(" w = %d, h = %d\n", w, h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
-  printf(" w = %d, h = %d\n", w, h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
