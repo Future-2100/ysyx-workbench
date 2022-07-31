1558f7aa1f04fcfe7801147bd731319951a54013
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 23:34:06 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:06 up 13:01,  0 users,  load average: 0.09, 0.05, 0.01

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -54,7 +54,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   printf(" line_off = %d\n ", line_off);
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
-    printf(" fp->ptr = 0x%x\n ", fp->_ptr);
+    printf(" fp->ptr = 0x%x\n ", fp->_p);
     printf(" w = %d\n ", w);
     printf(" i = %d\n ", i);
     int nread = fread(&pixels[w * i], 3, w, fp);
