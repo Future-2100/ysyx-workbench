075eaba37b0dd043d8801db6ea22e10533a10716
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 18:55:56 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:55:56 up  8:22,  0 users,  load average: 0.08, 0.02, 0.03

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,11 +67,9 @@ int fs_open(const char *pathname, int flags, int mode){
 
 size_t fs_read(int fd, void *buf, size_t len){
   if( file_table[fd].read == NULL ) {
-    /*
-    if( file_table[fd].open_offset + len >= file_table[fd].size ) {
+    if( file_table[fd].open_offset + len > file_table[fd].size ) {
       len = file_table[fd].size - file_table[fd].open_offset;
     }
-    */
 
     size_t offset = file_table[fd].open_offset + file_table[fd].disk_offset;
     ramdisk_read( buf, offset, len );
@@ -86,7 +84,7 @@ size_t fs_write(int fd, void *buf, size_t len){
   size_t offset = file_table[fd].open_offset + file_table[fd].disk_offset;
   if( file_table[fd].write == NULL ) {
     /*
-    if( file_table[fd].open_offset + len >= file_table[fd].size) {
+    if( file_table[fd].open_offset + len > file_table[fd].size) {
       len = file_table[fd].size - file_table[fd].open_offset;
     }
     */
