0279b32d8941e391ac0cdd009cc3801a676ce72f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 22:59:07 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:07 up 12:26,  0 users,  load average: 0.00, 0.02, 0.02

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -21,6 +21,7 @@ struct BitmapHeader {
 
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
+  printf(" fp->_file = %d\n", fp->file);
   if (!fp) return NULL;
 
   struct BitmapHeader hdr;
