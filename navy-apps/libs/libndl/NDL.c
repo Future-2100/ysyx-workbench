55229c72b3ad815877f99d87fa18d2d5044fb5ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 15:07:51 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:07:51 up  5:33,  0 users,  load average: 0.04, 0.13, 0.23

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -82,7 +82,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
       }
     }
   }
-
+  printf("i = %d, j = %d \n", i, j);
   assert(0);
   lseek(fp, 0 , SEEK_SET);
   write(fp, full_pixels, screen_w * screen_h);
