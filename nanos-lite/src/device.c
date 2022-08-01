d5da46227d40961e744907b0df2ed6e25046263d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 18:10:06 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:10:06 up  8:35,  0 users,  load average: 0.06, 0.03, 0.01

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -66,6 +66,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  printf("offset = %d, len = %d\n", offset, len);
 
   int width  = io_read(AM_GPU_CONFIG).width ;
   int height = io_read(AM_GPU_CONFIG).height;
