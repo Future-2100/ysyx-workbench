1a58368070d2461c968047e5f914fbd392af45ab
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 17:12:30 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:12:30 up  3:35,  0 users,  load average: 0.14, 0.03, 0.01

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H_
 #define __CONFIG_H_
 
-#define CONFIG_TRACE
+//#define CONFIG_TRACE
 //-------------------------some parameter related to the CONFIG_TRACE-----------------------
 #ifdef CONFIG_TRACE
     #define CONFIG_TRACE_START 0
@@ -11,7 +11,7 @@
 #endif
 //_______________________________end of the CONFIG_TRACE parameters--------------------------
 
-#define CONFIG_DIFFTEST
+//#define CONFIG_DIFFTEST
 //#define CONFIG_DEVICE
 
 #define CONFIG_PC_RESET_OFFSET 0x0
