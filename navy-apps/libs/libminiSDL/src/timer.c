06187ec5e70daaa7ce2fc4ca49b6ff438749e32b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 13:42:33 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:42:33 up 6 min,  0 users,  load average: 0.00, 0.01, 0.00

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -4,19 +4,23 @@
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
   printf("in ADDTimer");
+  assert(0);
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
   printf("in RemoveTimer");
+  assert(0);
   return 1;
 }
 
 uint32_t SDL_GetTicks() {
   printf("in GetTicks");
+  assert(0);
   return 0;
 }
 
 void SDL_Delay(uint32_t ms) {
   printf("in Delay");
+  assert(0);
 }
