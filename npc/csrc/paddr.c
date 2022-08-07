b3ed8dfa392459be219d79369decd70a53d7a42a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 11:58:58 +0800

    > sim RTL ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:58 up  2:12,  0 users,  load average: 0.16, 0.17, 0.12

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -37,16 +37,17 @@ extern VerilatedContext* contextp;
 static void out_of_bound(paddr_t addr) {
 
   //top->clk = !top->clk;
+  /*
   top->eval();
   contextp->timeInc(10);
   top->clk = !top->clk;
   top->eval();
   contextp->timeInc(10);
   delete_module();
-/*
+  */
+
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
               addr, (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE - 1, cpu.pc);
-              */
 
 }
 
