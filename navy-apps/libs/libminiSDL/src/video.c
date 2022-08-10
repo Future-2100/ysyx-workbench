8a01950934c24a247fcf7b6f15a7ae6795c055c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 22:43:35 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:35 up 12:46,  0 users,  load average: 0.08, 0.15, 0.11

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -13,7 +13,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  NDL_DrawRect( (uint32_t *)s->pixels, x, y, w, h);
+  NDL_DrawRect( s->pixels, x, y, w, h);
 }
 
 // APIs below are already implemented.
