e387bdaad62fe9aa1a744b9f061d81a1ea9bb4ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 22:19:45 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:45 up 12:22,  0 users,  load average: 0.08, 0.02, 0.08

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -6,30 +6,30 @@
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
-  assert(0);
+//  assert(0);
 };
 
 int SDL_PushEvent(SDL_Event *ev) {
-  assert(0);
+ // assert(0);
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  assert(0);
+  //assert(0);
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  assert(0);
+ // assert(0);
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  assert(0);
+ // assert(0);
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  assert(0);
+ // assert(0);
   return NULL;
 }
--- navy-apps/libs/libminiSDL/src/general.c
+++ navy-apps/libs/libminiSDL/src/general.c
@@ -1,30 +1,30 @@
 #include <NDL.h>
 
 int SDL_Init(uint32_t flags) {
-  assert(0);
+ // assert(0);
   return NDL_Init(flags);
 }
 
 void SDL_Quit() {
-  assert(0);
+//  assert(0);
   NDL_Quit();
 }
 
 char *SDL_GetError() {
-  assert(0);
+  //assert(0);
   return "Navy does not support SDL_GetError()";
 }
 
 int SDL_SetError(const char* fmt, ...) {
-  assert(0);
+ // assert(0);
   return -1;
 }
 
 int SDL_ShowCursor(int toggle) {
-  assert(0);
+  //assert(0);
   return 0;
 }
 
 void SDL_WM_SetCaption(const char *title, const char *icon) {
-  assert(0);
+ // assert(0);
 }
