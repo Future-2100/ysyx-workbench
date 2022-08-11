dc5025ac679a67f3a4b7ab0109b4a22c973d11d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 20:12:36 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:12:36 up  9:18,  0 users,  load average: 0.05, 0.03, 0.00

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -31,10 +31,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   int i,j;
   for( j = 0; j < h; j++) {
     for( i = 0; i < w; i++) {
+      printf("i = %d, j = %d\n",i, j);
       *( (uint32_t *)dst->pixels + (j+y)*dst->w + (i+x) ) = *( (uint32_t *)src->pixels + (j+srcrect->y)*src->w + (i+srcrect->x) ) ;
     }
   }
-  printf("end of SDL_BlitSurface\n");
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
