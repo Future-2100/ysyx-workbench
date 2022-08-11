15c31ba271faaf4d4fa3cab6180641bb5d867359
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 15:53:24 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:24 up  4:59,  0 users,  load average: 0.11, 0.05, 0.05

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -47,6 +48,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
       *ret = color;
       ret ++;
     } 
+    printf("reached here\n");
     NDL_DrawRect( (uint32_t *)pixels, dstrect->x, dstrect->y, dstrect->w, dstrect->h ) ;
   }
   
