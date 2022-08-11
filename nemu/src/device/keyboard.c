564e2443fb12703263fac6a75f63b9462d5f797c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 17:04:28 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:04:28 up  6:10,  0 users,  load average: 0.15, 0.16, 0.10

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -38,7 +38,7 @@ static int key_f = 0, key_r = 0;
 static void key_enqueue(uint32_t am_scancode) {
   key_queue[key_r] = am_scancode;
   key_r = (key_r + 1) % KEY_QUEUE_LEN;
-  //Assert(key_r != key_f, "key queue overflow!");
+  Assert(key_r != key_f, "key queue overflow!");
 }
 
 static uint32_t key_dequeue() {
