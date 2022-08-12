70f6d2d47a7234c78fccef604c0e541f9f96c36a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 15:34:56 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:34:56 up  6:02,  0 users,  load average: 0.22, 0.11, 0.03

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -131,14 +131,13 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     for( j = 0; j < h; j++) {
       for( i = 0; i < w; i++) {
         uint8_t index = *(s->pixels + (j+y)*s->w + i + x);
-        printf("index[%d][%d] = %d\n",i,j,*(s->pixels + i+x + (j+y)*s->w ));
-        printf("pixels[%d][%d] = %d\n",i,j,s->pixels[i+x+(j+y)*s->w]);
-
+        printf("index[%d][%d] = %d\n", i, j, index);
         uint8_t r = s->format->palette->colors[index].r ;
         uint8_t g = s->format->palette->colors[index].g ;
         uint8_t b = s->format->palette->colors[index].b ;
         uint8_t a = s->format->palette->colors[index].a ;
         color[i] = SDL_MapRGB(s->format, r, g, b, a );
+        printf("color = %x\n", color[i]);
         //color[i] = *((uint32_t *)s->format->palette->colors + (uintptr_t)*(s->pixels + (j+y)*s->w + i + x ) );
       }
       NDL_DrawRect( color, x, y + j , w, 1 );
