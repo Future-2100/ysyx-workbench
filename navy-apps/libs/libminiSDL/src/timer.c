2ab80c1524439a8ebaca896875d9536c0d028296
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 10:30:48 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:30:48 up 58 min,  0 users,  load average: 0.41, 0.27, 0.11

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
-  printf("in SDL_ADDTimer\n");
+//  printf("in SDL_ADDTimer\n");
 //  assert(0);
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
-  printf("in SDL_RemoveTimer\n");
+//  printf("in SDL_RemoveTimer\n");
 //  assert(0);
   return 1;
 }
@@ -19,6 +19,6 @@ uint32_t SDL_GetTicks() {
 }
 
 void SDL_Delay(uint32_t ms) {
-  printf("in SDL_Delay\n");
+//  printf("in SDL_Delay\n");
 //  assert(0);
 }
