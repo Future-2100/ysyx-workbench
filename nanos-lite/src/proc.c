57568d8d1452f5f0637be80b0663102f9a3961a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 10:58:57 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:57 up 47 min,  0 users,  load average: 0.23, 0.23, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,8 +29,9 @@ void init_proc() {
 
   // load program here
   //naive_uload(NULL, "/bin/bird");
-   naive_uload(NULL, "/bin/menu");
+  // naive_uload(NULL, "/bin/menu");
  //   naive_uload(NULL, "/bin/pal");
+ //   naive_uload(NULL, "/bin/nterm");
 //  naive_uload(NULL, NULL);
 }
 
