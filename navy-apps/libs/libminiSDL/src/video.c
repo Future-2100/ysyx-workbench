5b05683058742d0948690415c21797045b037e29
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 13:43:01 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:43:01 up  3:31,  0 users,  load average: 0.00, 0.01, 0.16

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -116,7 +116,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       }
       NDL_DrawRect( color, 0, j,  w, 1 );
     }
-    printf("finished 8 bits SDL_UpdateRect display\n");
+    //printf("finished 8 bits SDL_UpdateRect display\n");
   }
 }
 
