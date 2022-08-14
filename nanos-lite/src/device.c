5a9c5b17f10f16c2c3611f7324dd55d539f4fda6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 10:03:35 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:03:35 up 49 min,  0 users,  load average: 0.01, 0.05, 0.01

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -48,11 +48,11 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
+  printf("sizeof(buf): %d\n",sizeof(buf));
   printf("dispinfo_read : offset = %d, len = %d\n", offset, len);
   int width  = io_read(AM_GPU_CONFIG).width ;
   int height = io_read(AM_GPU_CONFIG).height;
   snprintf(buf, len, "WIDTH : %d \n HEIGHT : %d \n", width, height);
-  printf("buf : %s\n",buf);
   return 0;
 
 }
