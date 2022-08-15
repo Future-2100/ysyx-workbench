48e278f80b65096c64501799caf78a6f65180df3
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-15 11:03:48 +0800

    > compile NEMU ysyx_22040532 吴伟 Linux GrandQughtere 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:03:48 up  1:19,  0 users,  load average: 0.24, 0.44, 0.26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -71,8 +71,12 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   uint32_t *pixels = (uint32_t *)buf;
 
   io_write(AM_GPU_FBDRAW, start_x, start_y, pixels, width-start_x, 1, false);
+  pixels += width-start_x;
+
 
   io_write(AM_GPU_FBDRAW, 0, start_y + 1 , pixels, width, (end_y - start_y - 1), false);
+
+  pixels += ( width * (end_y - start_y - 1));
 
   io_write(AM_GPU_FBDRAW, 0, end_y, pixels, end_x, 1, false);
 
