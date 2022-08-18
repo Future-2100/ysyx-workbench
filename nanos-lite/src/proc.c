effc9fc1918d66358a0d312da7faae821c0b12e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 09:42:31 +0800

    > sim RTL ysyx_22040532 吴伟 Linux grand 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:42:31 up 2 min,  2 users,  load average: 0.08, 0.14, 0.06

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,9 +30,9 @@ void init_proc() {
   // load program here
 
 //naive_uload(NULL, "/bin/nslider");
-//naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/bin/menu");
 //naive_uload(NULL, "/bin/nterm");
-  naive_uload(NULL, "/bin/bird");
+//naive_uload(NULL, "/bin/bird");
 //naive_uload(NULL, "/bin/pal");
 //naive_uload(NULL, "/bin/bmp-test");
 //naive_uload(NULL, "/bin/timer-test");
